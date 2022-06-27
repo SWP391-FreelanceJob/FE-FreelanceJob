aab80a28f943a37afaf6d138372e94f1e81fb09e
nikethai <thong.thai7@gmail.com>
2022-06-27 18:13:00 +0700

    add JSDoc for message

---
+++ src/Api/Interface/IMessage.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef IMessage
+ * @prop {string} messageId
+ * @prop {IAccountMessage} fromAccount
+ * @prop {IAccountMessage} toAccount
+ * @prop {string} content
+ * @prop {string} attachFileUrl
+ * @prop {Date} sentTime
+ * @prop {string} status
+ * @prop {string} messageType
+ */
---
+++ src/Api/Interface/IRecruiterMessage.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef IRecruiterMessage
+ * @prop {number} id
+ * @prop {string} fullname
+ * @prop {string} shortDescription
+ * @prop {string} description
+ * @prop {string} avatar
+ * @prop {string} rating
+ */
