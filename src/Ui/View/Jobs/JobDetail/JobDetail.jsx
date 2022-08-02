1c4e4755e6c0480638c4790447b81e1ace554399
Thông <46261937+nikethai@users.noreply.github.com>
2022-08-02 11:28:22 +0700

    hot fix

--- src/Ui/View/Jobs/JobDetail/JobDetail.jsx
+++ src/Ui/View/Jobs/JobDetail/JobDetail.jsx
@@ -303,7 +303,7 @@ const JobDetail = () => {
                 </div>
               </div>
             </div>
-            {userState.role === "freelancer" && jobQuery.data.jobStatus == 0 && offerData && (
+            {userState.role === "freelancer" && jobQuery.data.jobStatus == 0 &&(
               <form className="relative" onSubmit={handleSubmit(onSubmit)}>
                 <div className="card card-compact all-shadow px-8 py-5">
                   <div className="flex justify-between">
